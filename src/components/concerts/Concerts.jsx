e0c4fd8b7a8ebdae4a82f80c0bc8b33bd58b938b
Jordi Roca <jordirocasoler94@gmail.com>
2022-07-21 22:32:40 +0200

    slider wip

--- src/components/concerts/Concerts.jsx
+++ src/components/concerts/Concerts.jsx
@@ -5,21 +5,65 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import {dataConcerts} from "./data";
 
+
 const Concerts = () => {
+
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1500,
+        settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+     
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
-    <div>
+    <div className='concerts__container'>
+    <Slider {...settings}>
         {dataConcerts.map((item)=>(
-        <div className="card">
-            <div className="card__top">
+        <div key={item.id} className="card">
             <img src={item.img} alt={item.name}/>
+            <div className='text__wrapper'>
             <h1>{item.name}</h1>
-            </div>
-            <div className="card_bottom">
             <h3>{item.date}</h3>
-            </div> 
+            </div>
         </div>
         ))}
-            
+    </Slider>
     </div>
   )
 }
--- src/components/concerts/data.js
+++ src/components/concerts/data.js
@@ -1,34 +1,76 @@
-import IMG1 from "../../assets/rayden.jpeg"
+import IMG1 from "../../assets/slider1.jpeg"
+import IMG2 from "../../assets/slider2.jpg"
+import IMG3 from "../../assets/slider3.png"
+import IMG4 from "../../assets/slider4.webp"
+import IMG5 from "../../assets/slider5.jpg"
+import IMG6 from "../../assets/slider6.jpg"
+import IMG7 from "../../assets/slider7.jpeg"
+import IMG8 from "../../assets/slider8.jpg"
+import IMG9 from "../../assets/slider9.jpeg"
+import IMG10 from "../../assets/slider10.avif"
+
+
+
 
 export const dataConcerts = [
     {
         id:1, 
-        name:"Rayden",
-        date:"Friday 24 September",
+        name:"RAYDEN",
+        date:"Friday 6 september",
         img:IMG1,
     },
     {
-        id:1, 
-        name:"Sean Paul",
-        date:"Friday 24 September",
-        img:IMG1,
+        id:2, 
+        name:"KODALINE",
+        date:"Friday 6 September",
+        img:IMG2,
     },
     {
-        id:1, 
-        name:"Sean Paul",
-        date:"Friday 24 September",
-        img:IMG1,
+        id:3, 
+        name:"JORDI RS",
+        date:"Sunday 8 September",
+        img:IMG3,
     },
     {
-        id:1, 
-        name:"Sean Paul",
-        date:"Friday 24 September",
-        img:IMG1,
+        id:4, 
+        name:"DUA LIPA",
+        date:"Sunday 8 September",
+        img:IMG4,
     },
     {
-        id:1, 
-        name:"Sean Paul",
-        date:"Friday 24 September",
-        img:IMG1,
+        id:5, 
+        name:"STROMAE",
+        date:"Saturday 7 September",
+        img:IMG5,
+    },
+    {
+        id:6, 
+        name:"BIZARRAP",
+        date:"Sunday 8 September",
+        img:IMG6,
+    },
+    {
+        id:7, 
+        name:"THE KILLERS",
+        date:"Saturday 7 September",
+        img:IMG7,
+    },
+    {
+        id:8, 
+        name:"DAVID GUETTA",
+        date:"Saturday 7 September",
+        img:IMG8,
+    },
+    {
+        id:9, 
+        name:"PAU VALLVÉ",
+        date:"Monday 9 September",
+        img:IMG9,
+    },
+    {
+        id:10, 
+        name:"BECKY G",
+        date:"Monday 9 September",
+        img:IMG10,
     },
 ]
