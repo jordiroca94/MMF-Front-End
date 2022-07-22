f1d1b888a49ca05564ba5b652cdfa03a2c4e18da
Jordi Roca <jordirocasoler94@gmail.com>
2022-07-22 13:15:51 +0200

    links added

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -22,20 +22,20 @@ const Sponsors = () => {
     <div className='sponsors__container'>
         <h1>Sponsors</h1>
         <div className='links__container'>
-            <a href="#"><img src={IMG1}/></a>
-            <a href="#"><img src={IMG2}/></a>
-            <a href="#"><img src={IMG3}/></a>
-            <a href="#"><img src={IMG4}/></a>
-            <a href="#"><img src={IMG5}/></a>
-            <a href="#"><img src={IMG6}/></a>
-            <a href="#"><img src={IMG7}/></a>
-            <a href="#"><img src={IMG8}/></a>
-            <a href="#"><img src={IMG9}/></a>
-            <a href="#"><img src={IMG10}/></a>
-            <a href="#"><img src={IMG11}/></a>
-            <a href="#"><img src={IMG12}/></a>
-            <a href="#"><img src={IMG13}/></a>
-            <a href="#"><img src={IMG14}/></a>
+            <a href="https://www.vueling.com/es" target="_blank" ><img src={IMG1}/></a>
+            <a href="https://www.estrelladamm.com/" target="_blank" ><img src={IMG2}/></a>
+            <a href="https://apps.apple.com/US/app/id547702041?mt=8" target="_blank"><img src={IMG3}/></a>
+            <a href="https://apps.apple.com/es/app/cabify/id476087442" target="_blank"><img src={IMG4}/></a>
+            <a href="https://heurafoods.com/" target="_blank"><img src={IMG5}/></a>
+            <a href="https://www.cocacola.es/" target="_blank"><img src={IMG6}/></a>
+            <a href="https://www.negrita.es/" target="_blank"><img src={IMG7}/></a>
+            <a href="https://www.redbull.com/es-es/" target="_blank"><img src={IMG8}/></a>
+            <a href="https://www.idealista.com/" target="_blank"><img src={IMG9}/></a>
+            <a href="https://www.jagermeister.com/" target="_blank"><img src={IMG10}/></a>
+            <a href="https://open.spotify.com/" target="_blank"><img src={IMG11}/></a>
+            <a href="https://www.fcbarcelona.es/es/" target="_blank"><img src={IMG12}/></a>
+            <a href="https://www.cupraofficial.es/" target="_blank"><img src={IMG13}/></a>
+            <a href="https://www.yamaha-motor.eu/" target="_blank"><img src={IMG14}/></a>
 
         </div>
 
