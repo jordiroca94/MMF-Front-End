07094419df9218db056126a2c04eb3ce4970af7c
Jordi Roca <jordirocasoler94@gmail.com>
2022-07-25 00:09:49 +0200

    responsive navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import "./navbar.css"
 import Logo from "../../assets/logo.jpg"
+import {FaBars,FaTimes} from "react-icons/fa";
 
 const Navbar = () => {
     const [activeNav,setActiveNav] = useState("#")
+    const navRef = useRef();
+
+    const showNavbar = ()=>{
+      navRef.current.classList.toggle("responsive_nav")
+    }
 
   return (
-    <nav className='navbar__container'>
-    <a href="#">
-    <img src={Logo} alt="Profile"/>
-    </a>
-    <div className='navbar__links__wrap'>
+    <header>
+      <a href="#">
+        <img src={Logo} alt="Profile"/>
+      </a>
+    <nav ref={navRef}>
         <a href='#tickets' onClick={()=> setActiveNav("#tickets")} className={activeNav === "#tickets" ? "active" : ""}>TICKETS</a>
         <a href='#camping' onClick={()=> setActiveNav("#camping")} className={activeNav === "#camping" ? "active" : ""}>CAMPING</a>
         <a href='#experience' onClick={()=> setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}>EXPERIENCE</a>
         <a href='#lineup' onClick={()=> setActiveNav("#lineup")} className={activeNav === "#lineup" ? "active" : ""}>LINE UP </a>
         <a href='#usefulinfo' onClick={()=> setActiveNav("#usefulinfo")} className={activeNav === "#usefulinfo" ? "active" : ""}>USEFUL INFO</a>
-    </div>
+        <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+          <FaTimes/>
+        </button>
     </nav>
+    <button className='nav-btn' onClick={showNavbar}><FaBars/></button>
+    </header>
   )
 }
 
