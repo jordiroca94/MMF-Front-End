6cd49c02f99d5dd0133efac0e727d808fa799a62
Jordi Roca <jordirocasoler94@gmail.com>
2022-08-02 19:33:01 +0200

    pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import { Route, Routes} from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Camping from './pages/camping/Camping';
+import Experience from './pages/experience/Experience';
 import Landing from './pages/landing/Landing'
+import Lineup from './pages/lineup/Lineup';
 import Tickets from './pages/tickets/Tickets';
+import UsefulInfo from './pages/useful-info/UsefulInfo';
 
 
 
@@ -16,6 +19,9 @@ const App = () => {
           <Route exact path="/" element={<Landing/>} />
           <Route exact path="/tickets" element={<Tickets/>} />
           <Route exact path="/camping" element={<Camping/>} />
+          <Route exact path="/experience" element={<Experience/>} />
+          <Route exact path="/lineup" element={<Lineup/>} />
+          <Route exact path="/usefulinfo" element={<UsefulInfo/>} />
         </Routes>
         <Footer/>
     </>
