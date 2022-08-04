b8d62cefee52f427ca143a7367615dc765502201
Jordi Roca <jordirocasoler94@gmail.com>
2022-08-04 15:45:32 +0200

    camping done

--- src/pages/camping/Camping.jsx
+++ src/pages/camping/Camping.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import NavbarCamping from '../../components/navbar-camping/NavbarCamping'
+import NavbarCamping from './NavbarCamping'
+import SectionsCamping from './SectionsCamping'
 import "./camping.css"
 
 const Camping = () => {
   return (
     <>     
       <NavbarCamping/>
+      <SectionsCamping/>
     </>
   )
 }
--- src/pages/camping/NavbarCamping.jsx
+++ src/pages/camping/NavbarCamping.jsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react'
-import "./navbarCamping.css"
-import CAMPING from "../../assets/camping/camping.jpg"
-import GLAMPING from "../../assets/camping/glamping.jpg"
-import RV from "../../assets/camping/rv.png"
-
+import "./camping.css"
 
 const NavbarCamping = () => {
-    const [activeNav, setActiveNav] = useState(null)
+    const [activeNav, setActiveNav] = useState("#")
 
   return (
     <>
@@ -15,14 +11,6 @@ const NavbarCamping = () => {
         <a href="#glamping" onClick={()=> setActiveNav("#glamping")} className={activeNav === "#glamping" ? "active" : ""}>GLAMPING</a>
         <a href="#rv" onClick={()=> setActiveNav("#rv")} className={activeNav === "#rv" ? "active" : ""}>RV</a>
     </div>
-
-    <section id="camping" className='h-full flex flex-col justify-center items-center '>
-        <h1 className='text-2xl p-4'>BRING YOUR OWN TENT</h1>
-        <img className='w-4/6' src={CAMPING}></img>
-    </section>
-    <section id="glamping" className='h-full'> THIS IS GLAMPING</section>
-    <section id="rv" className='h-full'> THIS IS RV</section>
-
   </>
   )
 }
