950d37247a6359dab8ededd893c27d908440f5cd
Jordi Roca <jordirocasoler94@gmail.com>
2022-08-04 12:58:19 +0200

    wip camp

--- src/components/navbar-camping/NavbarCamping.jsx
+++ src/components/navbar-camping/NavbarCamping.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import "./navbarCamping.css"
+import CAMPING from "../../assets/camping/camping.jpg"
+import GLAMPING from "../../assets/camping/glamping.jpg"
+import RV from "../../assets/camping/rv.png"
+
 
 const NavbarCamping = () => {
     const [activeNav, setActiveNav] = useState(null)
@@ -11,9 +15,13 @@ const NavbarCamping = () => {
         <a href="#glamping" onClick={()=> setActiveNav("#glamping")} className={activeNav === "#glamping" ? "active" : ""}>GLAMPING</a>
         <a href="#rv" onClick={()=> setActiveNav("#rv")} className={activeNav === "#rv" ? "active" : ""}>RV</a>
     </div>
-    <section id="camping"> THIS IS CAMPING</section>
-    <section id="glamping"> THIS IS GLAMPING</section>
-    <section id="rv"> THIS IS RV</section>
+
+    <section id="camping" className='h-full flex flex-col justify-center items-center '>
+        <h1 className='text-2xl p-4'>BRING YOUR OWN TENT</h1>
+        <img className='w-4/6' src={CAMPING}></img>
+    </section>
+    <section id="glamping" className='h-full'> THIS IS GLAMPING</section>
+    <section id="rv" className='h-full'> THIS IS RV</section>
 
   </>
   )
