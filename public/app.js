a9bee8f5d37f6b19b6d43d5e888d93ffed73468f
Jordi Roca <jordirocasoler94@gmail.com>
2022-08-05 19:45:27 +0200

    typescript basics

---
+++ public/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const msg = "Hi world";
+const hero = {
+    name: "Ironman",
+    age: 45,
+};
+hero.age = 50;
+console.log(hero.age);
---
+++ public/app.ts
@@ -0,0 +1,10 @@
+const msg: string = "Hi world"
+
+const hero = {
+    name:"Ironman",
+    age:45,
+}
+
+hero.age =50
+
+console.log(hero.age)
