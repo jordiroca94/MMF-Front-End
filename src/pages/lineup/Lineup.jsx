cf5e974bb58c2bfcb0f0a8759cef55fa78ba6bec
Jordi Roca <jordirocasoler94@gmail.com>
2022-08-09 23:45:57 +0200

    first day hardcoded

--- src/pages/lineup/Lineup.jsx
+++ src/pages/lineup/Lineup.jsx
@@ -4,22 +4,16 @@ import "./lineup.css";
 const Lineup = () => {
   return (
     <>
-      <div className='lineup__container'>
+      <div className='lineup__container '>
         <h1 className='text-white p-24 flex justify-center text-5xl '>LINEUP 2022 </h1>
-        <div className='p-2'>
-          <div className='flex justify-center text-white'>
-            <h3 className='flex-1'>Thursday 8th</h3>
-            <div className='flex-1'>
-              <p> C.TANGANA | STROMAE </p>
-              <p> BECKY G | LOLA INDIGO | RECYCLED G </p>
-              <p> RAYDEN | KODALINE | AITANA | DORIAN | LORY MEYERS </p>
-
-              
-
-              
+          <div className='flex flex-row justify-center gap-7 p-10'>
+            <h3 className='flex content-center px-6 py-4 text-white text-5xl'>Thursday 8th</h3>
+            <div className='px-6 py-4 text-white'>
+              <p className='text-3xl font-extrabold'> C.TANGANA | STROMAE </p>
+              <p className='text-2xl font-extrabold'> BECKY G | LOLA INDIGO | RECYCLED G </p>
+              <p className='text-xl font-extrabold'> RAYDEN | KODALINE | AITANA | DORIAN | LORY MEYERS </p>
             </div>
           </div>
-        </div>
       </div>
     </>
   )
