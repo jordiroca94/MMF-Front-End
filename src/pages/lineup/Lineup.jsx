95b2e732ae17f4d6ff8e235e473fbe9d4c117fb8
Jordi Roca <jordirocasoler94@gmail.com>
2022-08-09 19:56:38 +0200

    lineup wip

--- src/pages/lineup/Lineup.jsx
+++ src/pages/lineup/Lineup.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import "./lineup.css";
 
 const Lineup = () => {
   return (
-    <div>Lineup</div>
+    <>
+      <div className='lineup__container'>
+        <h1 className='text-white p-24 flex justify-center text-5xl '>LINEUP 2022 </h1>
+        <div className='p-2'>
+          <div className='flex justify-center text-white'>
+            <h3 className='flex-1'>Thursday 8th</h3>
+            <div className='flex-1'>
+              <p> C.TANGANA | STROMAE </p>
+              <p> BECKY G | LOLA INDIGO | RECYCLED G </p>
+              <p> RAYDEN | KODALINE | AITANA | DORIAN | LORY MEYERS </p>
+
+              
+
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
