d8bc2029531b675a0a551c40d355a32a4fa621eb
Jordi Roca <jordirocasoler94@gmail.com>
2022-08-10 12:26:54 +0200

    camping styled black

--- src/pages/camping/NavbarCamping.jsx
+++ src/pages/camping/NavbarCamping.jsx
@@ -6,7 +6,7 @@ const NavbarCamping = () => {
 
   return (
     <>
-    <div className='navbar__camping pt-8 pb-8 flex flex-row place-content-center gap-4'>
+    <div className='navbar__camping pt-8 pb-8 flex flex-row place-content-center gap-4 bg-red-700'>
         <a href="#camping" onClick={()=> setActiveNav("#camping")} className={activeNav === "#camping" ? "active" : ""}>CAMPING</a>
         <a href="#glamping" onClick={()=> setActiveNav("#glamping")} className={activeNav === "#glamping" ? "active" : ""}>GLAMPING</a>
         <a href="#rv" onClick={()=> setActiveNav("#rv")} className={activeNav === "#rv" ? "active" : ""}>RV</a>
